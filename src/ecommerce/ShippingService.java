65627c0609ee25233c06e80f5a662cb4b7d36fe5
Zeina20030 <155643900+Zeina20030@users.noreply.github.com>
2025-07-04 21:20:53 +0300

    Update ShippingService.java

--- src/ecommerce/ShippingService.java
+++ src/ecommerce/ShippingService.java
@@ -1,34 +1,33 @@
 package ecommerce;
 
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class ShippingService {
 
-    public static void ship(List<Shippable> items) {
+    public static void shipItems(List<Shippable> items) {
         System.out.println("** Shipment notice **");
 
         Map<String, ItemInfo> itemMap = new LinkedHashMap<>();
         double totalWeight = 0;
+        List<String> weightLabels = new ArrayList<>();
 
         for (Shippable item : items) {
-            Product product = (Product) item;
-            String name = product.getName();
+            String name = item.getName();
             double weight = item.getWeight();
 
             itemMap.putIfAbsent(name, new ItemInfo(0, weight));
             itemMap.get(name).count++;
             totalWeight += weight;
+
+            weightLabels.add((int)(weight * 1000) + "g");
         }
 
         for (Map.Entry<String, ItemInfo> entry : itemMap.entrySet()) {
-            String name = entry.getKey();
-            ItemInfo info = entry.getValue();
-            System.out.printf("%dx %-12s %.0fg%n", info.count, name, info.weight * 1000);
+            System.out.printf("%dx %-13s", entry.getValue().count, entry.getKey());
         }
 
-        System.out.printf("Total package weight %.1fkg%n", totalWeight);
+        System.out.println(String.join(" ", weightLabels));
+        System.out.printf("Total package weight %.1fkg\n", totalWeight);
     }
 
     private static class ItemInfo {
